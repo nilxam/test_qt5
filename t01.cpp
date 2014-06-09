c658a9a9910a8a19790068c27c010b4d6c12184d
Max Lin <mlin@suse.com>
2014-06-09 18:55:16 +0800

    test source

---
+++ t01.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QtCore>
+#include <QtGui>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+
+int main(int argv, char **args)
+{
+	QApplication app(argv, args);
+
+	QLabel *label = new QLabel("Hello, World!");
+	QPushButton *quitButton = new QPushButton("&OK");
+	quitButton->setDefault(true);
+
+	QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+	QVBoxLayout *layout = new QVBoxLayout;
+	layout->addWidget(label);
+	layout->addWidget(quitButton);
+
+	QWidget window;
+	window.setLayout(layout);
+
+	window.show();
+	window.raise();
+	window.update();
+
+	window.close();
+	//return app.exec();
+
+}
